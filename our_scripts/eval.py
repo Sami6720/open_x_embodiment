80912c91bae212726898c13081e4370aba883baa
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-03-24 23:01:04 -0400

    Fix formatting in eval.

--- our_scripts/eval.py
+++ our_scripts/eval.py
@@ -12,12 +12,15 @@ COLLECTED_EPISODES_SOURCE_DIR_PATH = '/home/dvenuto/af_dt/af_dt/language-table/d
 FRAMES_DESTINATION_DIR_PATH = '/home/dvenuto/af_dt/af_dt/language-table/language_table/frames'
 RESULTS_DIR = '/home/dvenuto/af_dt/af_dt/language-table/language_table/results'
 
+
 def decode_inst(inst):
-  """Utility to decode encoded language instruction"""
-  return bytes(inst[np.where(inst != 0)].tolist()).decode("utf-8")
+    """Utility to decode encoded language instruction"""
+    return bytes(inst[np.where(inst != 0)].tolist()).decode("utf-8")
+
 
 def get_episodes(target_episode_count, target_instruction):
-    source_dir_path = os.path.join(COLLECTED_EPISODES_SOURCE_DIR_PATH, '_'.join(target_instruction.split(' ')), str(target_episode_count))
+    source_dir_path = os.path.join(COLLECTED_EPISODES_SOURCE_DIR_PATH, '_'.join(
+        target_instruction.split(' ')), str(target_episode_count))
     if not os.path.exists(source_dir_path):
         print(f"""Either data for {target_instruction} has not been
               persisted or the exact target episode count {target_episode_count}
@@ -25,15 +28,19 @@ def get_episodes(target_episode_count, target_instruction):
               """)
     episodes = []
     for episode_num in range(target_episode_count):
-        episodes.append(tf.data.experimental.load(os.path.join(source_dir_path, str(episode_num))))
+        episodes.append(tf.data.experimental.load(
+            os.path.join(source_dir_path, str(episode_num))))
     return episodes
 
+
 def create_and_save_fig(rbg_array, instruction, file_ending):
-    frame_img_save_path = os.path.join(FRAMES_DESTINATION_DIR_PATH, f"{file_ending}.png")
+    frame_img_save_path = os.path.join(
+        FRAMES_DESTINATION_DIR_PATH, f"{file_ending}.png")
     plt.imshow(rbg_array.numpy())
     plt.title(decode_inst(instruction.numpy()))
     plt.savefig(frame_img_save_path)
     return frame_img_save_path
+
 
 def run_loop(episodes):
     checker = RealRobotics()
@@ -51,7 +58,8 @@ def run_loop(episodes):
                 print(print_statement)
                 f.write(print_statement)
                 observation = step['observation']
-                frame_img_path = create_and_save_fig(observation['rgb'], observation['instruction'], random_file_ending)
+                frame_img_path = create_and_save_fig(
+                    observation['rgb'], observation['instruction'], random_file_ending)
                 # TODO: Add GPT_CHECKER
                 reward_from_checker = checker.reward(frame_img_path)
                 print_statement = f"Reward by checker {reward_from_checker}\n"
@@ -60,13 +68,17 @@ def run_loop(episodes):
                 if step['is_last']:
                     checker.reset()
 
-if __name__=='__main__':
-   parser = argparse.ArgumentParser()
-   parser.add_argument('--target_episode_count', default=4, help="Number of episodes wanted.")
-   parser.add_argument('--target_instruction', default='push the red star to the bottom right of the board')
-   args = parser.parse_args()
-   episodes = get_episodes(args.target_episode_count, args.target_instruction)
-   run_loop(episodes)
 
-            # print(f"Start step: {step['is_first']}")
-            # print(f"Last step: {step['is_last']}")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target_episode_count', default=4,
+                        help="Number of episodes wanted.")
+    parser.add_argument('--target_instruction',
+                        default='push the red star to the bottom right of the board')
+    args = parser.parse_args()
+    episodes = get_episodes(args.target_episode_count, args.target_instruction)
+    run_loop(episodes)
+
+    # print(f"Start step: {step['is_first']}")
+    # print(f"Last step: {step['is_last']}")
+
